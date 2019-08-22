35ad5a9c30b700ebfc3736369bfc095fdb917d93
Lukas Veneziano <fs@venezilu.de>
2019-08-22 13:35:31 +0200

    rebuild everything hehe

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-if [ -d "sharedModsVolume" ] && [ -n "$(ls -A "sharedModsVolume")" ]; then
-    echo "copy mods from volume to actual sharedMods" 
-    cp -Rv sharedModsVolume/* sharedMods/
+#if [ -d "sharedModsVolume" ] && [ -n "$(ls -A "sharedModsVolume")" ]; then
+#    echo "copy mods from volume to actual sharedMods" 
+#    cp -Rv sharedModsVolume/* sharedMods/
+#
+#else
+#    echo "no shared mods from outside docker"
+#fi
 
-else
-    echo "no shared mods from outside docker"
+# instead of copying this install clusterio to volume
+if [ -d "sharedMods" ]; then
+    mkdir sharedMods
 fi
-if [ -d "sharedPluginsVolume" ] && [ -n "$(ls -A "sharedPluginsVolume")" ]; then
-    echo "copy pluginss from volume to actual sharedMods" 
-    cp -Rv sharedPluginsVolume/* sharedPlugins/
+node client.js download
+
+if [ -d "sharedPlugins" ] && [ -n "$(ls -A "sharedPlugins")" ]; then
+    echo "copy plugins from git to actual sharedMods" 
+    cp -Rv sharedPluginsDist/* sharedPlugins/
 else
     echo "no shared plugins from outside docker"
+    mv sharedPluginsDist sharedPlugins
 fi
 
 # Hand off to the CMD
